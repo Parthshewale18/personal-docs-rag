5b91ae99fbfd097b4e947542efb715396c49ff4d
Parthshewale18 <shewaleparth19@gmail.com>
2026-05-18 10:07:07 +0530

    Initial Commit

---
+++ ingest.py
@@ -0,0 +1,34 @@
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_chroma import Chroma
+
+def load_pdf(file_path):
+    loader = PyPDFLoader(file_path)
+    documents = loader.load()
+    return documents
+
+
+def split_text(document, chunk_size=1000, chunk_overlap=200):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=chunk_size,
+        chunk_overlap=chunk_overlap
+    )
+    texts = text_splitter.split_documents(document)
+    return texts
+
+
+def create_vector_store(texts, embedding_model="all-MiniLM-L6-v2"):
+    embeddings = HuggingFaceEmbeddings(model_name=embedding_model)
+    vector_store = Chroma.from_documents(
+        documents=texts,
+        embedding=embeddings,
+        persist_directory="./chroma_db"
+        )
+    return vector_store
+
+if __name__ == "__main__":
+    file_path = "Tesla_EDA_Project_Report.pdf"
+    document = load_pdf(file_path)
+    texts = split_text(document)
+    vector_store = create_vector_store(texts)
